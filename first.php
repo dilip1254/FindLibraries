67d8c2b1fc5b0d38f1e43a9804c4570a19d50bf3
dilip1254-Git <198r1a1254@gmail.com>
2022-05-27 00:05:33 +0530

    first commit

---
+++ first.php
@@ -0,0 +1,16 @@
+<h1>
+<?php
+$abc = "Hello World";
+function a(){
+    $abc = "hey";
+    echo $abc;
+}
+function b(){
+    $abc = "Hi";
+    echo $abc;
+}
+a();
+b();
+echo $abc;
+?>
+</h1>
---
+++ js/app.js
@@ -0,0 +1,1 @@
+const my =   (<p> Three plus two is {3+2} </p>);
